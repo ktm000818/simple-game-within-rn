b5c16c4ee6a9be29b7533497d64d651a0bd4aab1
taemin kim <97078686+ktm000818@users.noreply.github.com>
2024-03-23 21:11:17 +0900

    Feat: 폰트 추가 및 Splash 적용 (Fallback ui)

--- App.js
+++ App.js
@@ -1,28 +1,68 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { ImageBackground, SafeAreaView, StyleSheet, View } from "react-native";
-import HomeScreen from "./screens/HomeScreen";
 import { LinearGradient } from "expo-linear-gradient";
-import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useEffect, useState } from "react";
+import { ImageBackground, SafeAreaView, StyleSheet } from "react-native";
+import Colors from "./constants/colors";
+import GameOverScreen from "./screens/GameOverScreen";
 import GameScreen from "./screens/GameScreen";
+import HomeScreen from "./screens/HomeScreen";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isAppReady, setIsAppReady] = useState(false);
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+          "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+        });
+      } catch (error) {
+        console.warn(error)
+      } finally {
+        setIsAppReady(true);
+      }
+    }
+
+    prepare();
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if(isAppReady){
+      await SplashScreen.hideAsync();
+    }
+  }, [isAppReady])
 
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
   }
 
   let screen = <HomeScreen getPickedNumber={pickedNumberHandler} />;
-
   if (userNumber) {
-    screen = <GameScreen />;
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
+  }
+  if (gameIsOver) {
+    screen = <GameOverScreen />;
+  }
+
+  function gameOverHandler() {
+    setGameIsOver(true);
+  }
+
+  if(!isAppReady){
+    return null;
   }
 
   return (
     <>
       <StatusBar style="light" />
-      <LinearGradient colors={["#640131", "#ddb52f"]} style={styles.rootContainer}>
+      <LinearGradient colors={[Colors.primary600, Colors.secondary500]} style={styles.rootContainer} onLayout={onLayoutRootView}>
         <ImageBackground imageStyle={styles.backgroundImage} source={require("./assets/images/background.png")} resizeMode="cover" style={styles.rootContainer}>
           <SafeAreaView style={styles.rootContainer}>{screen}</SafeAreaView>
         </ImageBackground>
